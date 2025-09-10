0d2e9ebec1930b452fdc69da3db5b2ff523399ae
Ivan Nikerin <nia@arenadata.io>
2025-09-10 11:42:14 +0300

    init trino

--- trino_iceberg_query_dag.py
+++ trino_iceberg_query_dag.py
@@ -18,22 +18,7 @@ with DAG(
         split_statements=True,  # Important for multiple statements
     )
 
-    # # Define another TrinoOperator task to execute an INSERT statement
-    # trino_insert_task = TrinoOperator(
-    #     task_id='execute_trino_insert',
-    #     sql="INSERT INTO my_catalog.my_schema.another_table (col1, col2) VALUES ('value1', 'value2');",
-    #     trino_conn_id='trino_default',
-    #     autocommit=True,  # Set to True for DML statements like INSERT
-    # )
-    #
-    # # You can also execute SQL from a file using a Jinja template
-    # trino_from_file_task = TrinoOperator(
-    #     task_id='execute_trino_from_file',
-    #     sql='templates/my_trino_query.sql',  # Path to a .sql file in your DAGs folder
-    #     trino_conn_id='trino_default',
-    #     parameters={'start_date': '{{ ds }}', 'end_date': '{{ next_ds }}'}, # Example of passing parameters
-    # )
 
     # Define task dependencies
-    trino_select_task
+    execute_trino_query
     #>> trino_insert_task >> trino_from_file_task
