6634eb36f5f58886c8fbede80eecb6d93f1cbf77
Ivan Nikerin <nia@arenadata.io>
2025-09-10 19:37:25 +0300

    init adb master

--- adb_maser_query_dag.py
+++ adb_maser_query_dag.py
@@ -3,74 +3,74 @@ from datetime import datetime, timedelta
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 with DAG(
-    dag_id='trino_iceberg_query',
+    dag_id='adb_master_query',
     start_date=datetime(2025, 9, 9),
     schedule='@once',
     catchup=False,
-    tags=['trino', 'example'],
+    tags=['adb', 'example'],
 ) as dag:
     execute_drop_shema_trino_query = SQLExecuteQueryOperator(
-        task_id='drop_schema_iceberg',
-        sql="DROP SCHEMA IF EXISTS iceberg.example CASCADE;",
-        conn_id='sel-dev-trino-iceberg',
+        task_id='drop_schema_adb_master',
+        sql="DROP SCHEMA IF EXISTS example CASCADE;",
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_create_shema_trino_query = SQLExecuteQueryOperator(
-        task_id='create_schema_iceberg',
-        sql="CREATE SCHEMA IF NOT EXISTS iceberg.example;",
-        conn_id='sel-dev-trino-iceberg',
+        task_id='create_schema_adb_master',
+        sql="CREATE SCHEMA IF NOT EXISTS example;",
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_drop_table_trino_query = SQLExecuteQueryOperator(
-        task_id='drop_table_iceberg',
-        sql="DROP TABLE IF EXISTS iceberg.example.names;",
-        conn_id='sel-dev-trino-iceberg',
+        task_id='drop_table_adb_master',
+        sql="DROP TABLE IF EXISTS example.names;",
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_create_table_trino_query = SQLExecuteQueryOperator(
-        task_id='create_table_iceberg',
+        task_id='create_table_adb_master',
         sql="""
-        CREATE TABLE iceberg.example.names (
+        CREATE TABLE example.names (
         id INT,
         name VARCHAR,
         timestamp VARCHAR
         );
         """,
-        conn_id='sel-dev-trino-iceberg',
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_incert_to_table_trino_query = SQLExecuteQueryOperator(
-        task_id='incert_to_table_iceberg',
+        task_id='incert_to_table_adb_master',
         sql="""
         INSERT INTO iceberg.example.names VALUES
         (1, 'Alice', '2025-01-01 10:00:00'),
         (2, 'Bob', '2025-01-02 11:30:00');
         """,
-        conn_id='sel-dev-trino-iceberg',
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_select_table_trino_query = SQLExecuteQueryOperator(
-        task_id='select_from_table_iceberg',
-        sql="SELECT * FROM iceberg.example.names;",
-        conn_id='sel-dev-trino-iceberg',
+        task_id='select_from_table_adb_master',
+        sql="SELECT * FROM example.names;",
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
 
     execute_create_as_select_table_trino_query = SQLExecuteQueryOperator(
-        task_id='create_as_select_from_table_iceberg',
-        sql="CREATE TABLE iceberg.example.names_from AS SELECT * FROM iceberg.example.names;",
-        conn_id='sel-dev-trino-iceberg',
+        task_id='create_as_select_from_table_adb_master',
+        sql="CREATE TABLE example.names_from AS SELECT * FROM example.names;",
+        conn_id='sel-dev-master-adb',
         autocommit=True,
         split_statements=True,
     )
