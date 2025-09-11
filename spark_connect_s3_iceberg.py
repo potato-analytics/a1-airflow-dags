b5304e5caeab0715e7b4090f6eb5c44d1064b6d4
Ivan Nikerin <nia@arenadata.io>
2025-09-11 18:39:18 +0300

    init spark connect

--- spark_connect_s3_iceberg.py
+++ spark_connect_s3_iceberg.py
@@ -26,13 +26,13 @@ with DAG(
 ) as dag:
     submit_spark_job = SparkSubmitOperator(
         task_id='spark_connect_s3_iceberg_read',
-        application='s3a://demo-bucket/pyspark/01_spark_read_iceberg.py',  # Or .jar file
-        conn_id='sel-dev-spark-connect',  # The Connection Id you defined
+        application='s3a://demo-bucket/pyspark/01_spark_read_iceberg.py',
+        conn_id='sel-dev-spark-connect',
         conf={'spark.hadoop.fs.s3a.access.key': access_key,
               "spark.hadoop.fs.s3a.secret.key": secret_key,
-              "spark.hadoop.fs.s3a.endpoint": endpoint},  # Optional Spark configurations
+              "spark.hadoop.fs.s3a.endpoint": endpoint},
         application_args=['--input_access_key ' + external_access_key,
                           '--input_secret_key ' + external_secret_key,
                           '--input_endpoint ' + external_endpoint,
-                          '--input_bucket ' + external_bucket],  # Arguments for your Spark application
+                          '--input_bucket ' + external_bucket],
     )
