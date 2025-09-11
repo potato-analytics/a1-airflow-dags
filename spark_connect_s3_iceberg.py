aab44fa59571a2c96a17a3f12bab64c3959d7683
Ivan Nikerin <nia@arenadata.io>
2025-09-11 17:18:47 +0300

    init spark connect

--- spark_connect_s3_iceberg.py
+++ spark_connect_s3_iceberg.py
@@ -19,7 +19,6 @@ with DAG(
         task_id='spark_connect_s3_iceberg_read',
         application='s3a://demo-bucket/pyspark/01_spark_read_iceberg.py',  # Or .jar file
         conn_id='sel-dev-spark-connect',  # The Connection Id you defined
-        packages='org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.901', # Example for additional packages
         #conf={'spark.executor.memory': '4g'},  # Optional Spark configurations
         #application_args=['arg1', 'arg2'],  # Arguments for your Spark application
     )
