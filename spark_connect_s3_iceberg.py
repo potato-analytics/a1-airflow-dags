71e0b92bdd11331528f24ec69d84a13bd6b97d33
Ivan Nikerin <nia@arenadata.io>
2025-09-12 12:19:03 +0300

    init spark connect

--- spark_connect_s3_iceberg.py
+++ spark_connect_s3_iceberg.py
@@ -2,56 +2,52 @@ from airflow import DAG
 from airflow.hooks.base import BaseHook
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
-from airflow.providers.apache.spark.hooks.spark_connect import SparkConnectHook
-import json
+# from airflow.providers.apache.spark.hooks.spark_connect import SparkConnectHook
+# import json
+#
+# s3_connection = BaseHook.get_connection('sel-dev-s3-logs')
+#
+# access_key = s3_connection.login
+# secret_key = s3_connection.password
+# extra_data = json.loads(s3_connection.extra)
+# endpoint = extra_data.get("endpoint")
+#
+# s3_external_connection = BaseHook.get_connection('vk-external-s3')
+# external_access_key = s3_external_connection.login
+# external_secret_key = s3_external_connection.password
+# external_extra_data = json.loads(s3_external_connection.extra)
+# external_endpoint = external_extra_data.get("endpoint")
+# external_bucket = external_extra_data.get("bucket")
+#
+# sc_spark_connection = BaseHook.get_connection('sel-dev-spark-connect')
+# print("=====================")
+# print(BaseHook.get_connection('sel-dev-spark-connect'))
+# print("=====================")
+# sc_spark_connection_extra_data = json.loads(sc_spark_connection.extra)
+# sc_url = sc_spark_connection_extra_data.get("sc_url")
+# print(sc_url)
+# print("=====================")
+# print(sc_spark_connection.conn_type)
+# print(SparkConnectHook.conn_type)
+# print(SparkConnectHook("sel-dev-spark-connect").get_connection_url())
+# print("=====================")
 
-s3_connection = BaseHook.get_connection('sel-dev-s3-logs')
-
-access_key = s3_connection.login
-secret_key = s3_connection.password
-extra_data = json.loads(s3_connection.extra)
-endpoint = extra_data.get("endpoint")
-
-s3_external_connection = BaseHook.get_connection('vk-external-s3')
-external_access_key = s3_external_connection.login
-external_secret_key = s3_external_connection.password
-external_extra_data = json.loads(s3_external_connection.extra)
-external_endpoint = external_extra_data.get("endpoint")
-external_bucket = external_extra_data.get("bucket")
-
-sc_spark_connection = BaseHook.get_connection('sel-dev-spark-connect')
-print("=====================")
-print(BaseHook.get_connection('sel-dev-spark-connect'))
-print("=====================")
-sc_spark_connection_extra_data = json.loads(sc_spark_connection.extra)
-sc_url = sc_spark_connection_extra_data.get("sc_url")
-print(sc_url)
-print("=====================")
-print(sc_spark_connection.conn_type)
-print(SparkConnectHook.conn_type)
-print(SparkConnectHook("sel-dev-spark-connect").get_connection_url())
-print("=====================")
 with DAG(
     dag_id='spark_connect_s3_iceberg',
     start_date=datetime(2023, 1, 1),
     schedule='@once',
     catchup=False,
 ) as dag:
-    print("=====================")
-    print(sc_spark_connection.conn_type)
-    print(SparkConnectHook.conn_type)
-    print(SparkConnectHook("sel-dev-spark-connect").get_connection_url())
-    print("=====================")
     submit_spark_job = SparkSubmitOperator(
         task_id='spark_connect_s3_iceberg_read',
         application='s3a://demo-bucket/pyspark/01_spark_read_iceberg.py',
         conn_id='sel-dev-spark-connect',
-        conf={'spark.hadoop.fs.s3a.access.key': access_key,
-              "spark.hadoop.fs.s3a.secret.key": secret_key,
-              "spark.hadoop.fs.s3a.endpoint": endpoint},
-        application_args=[external_access_key,
-                          external_secret_key,
-                          external_endpoint,
-                          external_bucket,
-                          sc_url],
-    )
+        conf={'spark.hadoop.fs.s3a.access.key': "1703b531c6154229b2ce27848703b84d",
+              "spark.hadoop.fs.s3a.secret.key": "af7afab662d14b2890402caf3a76153d",
+              "spark.hadoop.fs.s3a.endpoint": "s3.ru-7.storage.selcloud.ru"}
+    #     application_args=[external_access_key,
+    #                       external_secret_key,
+    #                       external_endpoint,
+    #                       external_bucket,
+    #                       sc_url],
+    # )
