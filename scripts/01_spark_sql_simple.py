ec6c64707b6c7f317081226bb76b76f9c1b470c0
Ivan Nikerin <nia@arenadata.io>
2025-09-12 13:43:53 +0300

    init spark connect

--- scripts/01_spark_sql_simple.py
+++ scripts/01_spark_sql_simple.py
@@ -1,9 +1,6 @@
 import sys
 
-
 sc_url = sys.argv[1]
-
-
 
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
@@ -12,6 +9,8 @@ spark = SparkSession.builder.remote(sc_url) \
     .appName("Airflow_SparkConnect_Read_From_other_S3_bucket") \
     .getOrCreate()
 
-spark.sql("select 1").show(1,0)
+spark.sql("CREATE TABLE default.spark_connect_example (id INT)").show(1,0)
+
+spark.sql("select * from default.spark_connect_example").show(1,0)
 
 spark.sparkContext.stop()
